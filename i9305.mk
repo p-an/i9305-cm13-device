86e3a93bae9d4b2d845798a358bebfd157bd103d
Pavel <pavel.andrejs@gmail.com>
2016-01-15 15:34:10 +0000

    android.hardware.sensor.barometer.xml added
    Change-Id: Icca53cb7327d6403eefb19ef3627ab9eec9163cf

--- i9305.mk
+++ i9305.mk
@@ -94,7 +94,8 @@ PRODUCT_COPY_FILES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
 
 # Allow tethering without provisioning app
 PRODUCT_PROPERTY_OVERRIDES += \
