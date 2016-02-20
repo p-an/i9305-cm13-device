482d1559f5d231a942a94c89d80d92a4f4ed69a9
Pavel <pavel.andrejs@gmail.com>
2016-02-20 13:10:51 +0000

    new select ril script

--- rilchroot/Android.mk
+++ rilchroot/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := rilchroot
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT)/rilchroot
 
-$(TARGET_OUT)/rilchroot: $(TARGET_OUT)/cm12chroot $(TARGET_OUT)/vrtchroot $(TARGET_OUT)/xbin/select_ril.sh
+$(TARGET_OUT)/rilchroot: $(TARGET_OUT)/cm12chroot $(TARGET_OUT)/vrtchroot $(TARGET_OUT)/xbin/ril
 	ln -sf cm12chroot $(TARGET_OUT)/rilchroot
 
 $(TARGET_OUT)/cm12chroot:
@@ -12,7 +12,7 @@ $(TARGET_OUT)/cm12chroot:
 $(TARGET_OUT)/vrtchroot:
 	tar xzf device/samsung/i9305/rilchroot/vrtchroot.tgz -C $(TARGET_OUT)
 
-$(TARGET_OUT)/xbin/select_ril.sh:
-	cp device/samsung/i9305/rilchroot/select_ril.sh $(TARGET_OUT)/xbin/select_ril.sh
+$(TARGET_OUT)/xbin/ril:
+	cp device/samsung/i9305/rilchroot/ril $(TARGET_OUT)/xbin/ril
 
 include $(BUILD_PHONY_PACKAGE)
