9b333b19b8203a73469632e750db7c2b0b52f9cf
Pavel <pavel.andrejs@gmail.com>
2016-02-20 22:19:37 +0000

    remove vrt from makefile and selec ril script

--- rilchroot/Android.mk
+++ rilchroot/Android.mk
@@ -3,14 +3,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := rilchroot
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT)/rilchroot
 
-$(TARGET_OUT)/rilchroot: $(TARGET_OUT)/cm12chroot $(TARGET_OUT)/vrtchroot $(TARGET_OUT)/xbin/ril
+$(TARGET_OUT)/rilchroot: $(TARGET_OUT)/cm12chroot $(TARGET_OUT)/xbin/ril
 	ln -sf cm12chroot $(TARGET_OUT)/rilchroot
 
 $(TARGET_OUT)/cm12chroot:
 	tar xzf device/samsung/i9305/rilchroot/cm12chroot.tgz -C $(TARGET_OUT)
-
-$(TARGET_OUT)/vrtchroot:
-	tar xzf device/samsung/i9305/rilchroot/vrtchroot.tgz -C $(TARGET_OUT)
 
 $(TARGET_OUT)/xbin/ril: $(TARGET_OUT)/xbin
 	cp device/samsung/i9305/rilchroot/ril $(TARGET_OUT)/xbin/ril
