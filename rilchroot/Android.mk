1f7f875aaf5ebc47185c073cdca1e39ee4eea1ad
Pavel <pavel.andrejs@gmail.com>
2016-02-20 13:49:10 +0000

    build fix

--- rilchroot/Android.mk
+++ rilchroot/Android.mk
@@ -12,7 +12,10 @@ $(TARGET_OUT)/cm12chroot:
 $(TARGET_OUT)/vrtchroot:
 	tar xzf device/samsung/i9305/rilchroot/vrtchroot.tgz -C $(TARGET_OUT)
 
-$(TARGET_OUT)/xbin/ril:
+$(TARGET_OUT)/xbin/ril: $(TARGET_OUT)/xbin
 	cp device/samsung/i9305/rilchroot/ril $(TARGET_OUT)/xbin/ril
+
+$(TARGET_OUT)/xbin:
+	mkdir -p $(TARGET_OUT)/xbin
 
 include $(BUILD_PHONY_PACKAGE)
